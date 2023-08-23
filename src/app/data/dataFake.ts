744fa22118bd0560de4517aa30fb5f1046d489ee
José Augusto <joseaugustogomessantos12@gmail.com>
2023-08-23 18:34:06 -0300

    conteudo exibido por id

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,15 +1,27 @@
 export const dataFake = [
   {
-    "id":1,
-    "photo": 'dad',
-    "title":'Saiu um novo filme',
-    "description": 'Olá'
+    "id":'1',
+    "title":'NOVO FILME DO HOMEM DE FERRO 3D',
+    "description": 'Marvel studios anuncia novo filme do homem de ferro.',
+    "photoCover": "https://variety.com/wp-content/uploads/2013/03/ironman3_poster.jpg?w=800"
   },
   {
-    "id":2,
-    "photo": 'dad',
-    "title":'Saiu um novo filme',
-    "description": 'Olá'
+    "id":'2',
+    "title":'Saiu novos filmes do universo marvel',
+    "description": 'Novos filmes anunciados',
+    "photoCover": "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  },
+  {
+    "id":'3',
+    "title":'Saiu novo Filme do pantera negra',
+    "description": 'Filme do pantera negra é anunciado',
+    "photoCover": "https://i0.wp.com/metagalaxia.com.br/wp-content/uploads/2022/11/pantera-negra-1.webp?fit=1920%2C1080&ssl=1"
+  },
+  {
+    "id":'4',
+    "title":'Saiu novo filme do homem aranha',
+    "description": 'Novos filme do homem aranha é anunciado',
+    "photoCover": "https://portalpopline.com.br/wp-content/uploads/2023/06/homem-aranha-4-2025-758x570.jpg"
   },
 ]
 
